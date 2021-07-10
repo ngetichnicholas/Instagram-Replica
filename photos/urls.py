2808303d434fb0e1bea7a73e80ab5cf67c6a1d42
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-10 10:45:54 +0300

    Add app urls

---
+++ photos/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+  url(r'^$',views.home,name='home'),
+  url('^search/', views.search, name='search'),
+  url(r'^follow/(\d+)', views.follow, name='follow'),
+  url(r'^unfolow/(\d+)', views.unfollow, name='unfollow'),
+  url(r'^subscribe/', views.subscribe, name='subscribe'),
+  url(r'^accounts/profile', views.profile,name='user-profile'),
+
+]
