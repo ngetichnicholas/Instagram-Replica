fb19258580b551b8114925ac43d695de9e45f841
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-10 10:42:45 +0300

    Add static files

---
+++ photos/static/js/script.js
@@ -0,0 +1,14 @@
+
+function imgWindow() {
+  window.open("image")
+}
+$(document).ready(function () {
+  $(function () {
+    $('.photoUrl').click(function () {
+      $(this).focus();
+      $(this).select();
+      document.execCommand('copy');
+      alert("Copied to clipboard");
+    });
+  });
+});
