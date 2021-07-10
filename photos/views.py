7325610532ab9567aa15d8b3e3b47b2e5c393bd9
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-10 19:09:08 +0300

    Add people sections display

--- photos/views.py
+++ photos/views.py
@@ -2,14 +2,15 @@ from photos.forms import SearchForm
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404,HttpResponseRedirect
-from .models import Image
+from .models import Image, Profile
 
 
 # Create your views here.
 def index(request):
   search_form = SearchForm()
   photos = Image.objects.all().order_by("-posted_at")
-  return render(request,'index.html',{'photos':photos,'form':search_form})
+  people = Profile.objects.all().order_by("-username")
+  return render(request,'index.html',{'photos':photos,'people':people,'form':search_form})
 
 def search(request):
   return redirect('home')
