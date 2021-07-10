79315357d0b56b6d243af4dde1a07b6d5dd9cdc5
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-10 14:20:06 +0300

    Create Image model class

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
+from django.db.models.fields import CharField, TextField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class Profile(models.Model):
+  pass
+
+class Image(models.Model):
+  image = CloudinaryField('image')
+  image_name = CharField(max_length=30)
+  image_caption = CharField(max_length=144)
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  likes = CharField(max_length=10)
+  comments = TextField()
+
+
+
