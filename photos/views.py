ae95c1ad306cb9fbb3411220bbb9e0e8e072f7cb
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-10 10:52:55 +0300

    Add basic index view

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+
 
 # Create your views here.
+def index(request):
+  return render('index.html')
