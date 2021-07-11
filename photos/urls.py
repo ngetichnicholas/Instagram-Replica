14a57ad496bda3809afd4f18429a9e54190f4f27
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-11 13:57:46 +0300

    Update urls and links

--- photos/urls.py
+++ photos/urls.py
@@ -7,11 +7,10 @@ urlpatterns = [
   url('^search/', views.search, name='search'),
   url(r'^post/$', views.post, name='post'),
   url(r'^subscribe/', views.subscribe, name='subscribe'),
-  url(r'^accounts/profile', views.profile,name='user-profile'),
-  url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
-  url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
-  url(r'^follow/(?P<user_id>\d+)', views.follow, name='follow'),
+  url(r'^comment/(\d+)', views.comment, name='comment'),
+  url(r'^like/(\d+)', views.like, name='like'),
+  url(r'^follow/(\d+)', views.follow, name='follow'),
   url(r'^update-profile/',views.update_profile,name ='update-profile'),
-  url(r'^profile/(?P<id>\d+)',views.profile,name = 'profile'),
+  url(r'^accounts/profile/(\d+)',views.profile,name = 'profile'),
 
 ]
