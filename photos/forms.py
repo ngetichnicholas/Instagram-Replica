2f7bd15a1dcbf829721bb72516aef8b80069bb2b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-11 13:32:09 +0300

    Update photos urls

--- photos/forms.py
+++ photos/forms.py
@@ -1,11 +1,21 @@
 from django import forms
-from .models import Profile,Image
+from .models import Profile,Image,Comment
 
 
 class SearchForm(forms.Form):
-    search_field = forms.CharField(label='', max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Search people...'}))
+  search_field = forms.CharField(label='', max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Search people...'}))
 
 class PostForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        exclude =['likes','profile']
+  class Meta:
+    model = Image
+    exclude =['likes','profile']
+
+class ProfileForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    exclude=['username']
+
+class CommentForm(forms.ModelForm):
+  class Meta:
+    model = Comment
+    exclude = ['photo', 'comment_username']
--- photos/urls.py
+++ photos/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
   url(r'^$',views.index,name='home'),
   url('^search/', views.search, name='search'),
   url(r'^post/$', views.post, name='post'),
-  url(r'^follow/(\d+)', views.follow, name='follow'),
-  url(r'^unfolow/(\d+)', views.unfollow, name='unfollow'),
   url(r'^subscribe/', views.subscribe, name='subscribe'),
   url(r'^accounts/profile', views.profile,name='user-profile'),
+  url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+  url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
+  url(r'^follow/(?P<user_id>\d+)', views.follow, name='follow'),
+  url(r'^update-profile/',views.update_profile,name ='update-profile'),
+  url(r'^profile/(?P<id>\d+)',views.profile,name = 'profile'),
 
 ]
