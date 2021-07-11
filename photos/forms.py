da64c79478f5030344045a940fcc698928e6c92b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-11 12:48:58 +0300

    Add form to add a post

--- photos/forms.py
+++ photos/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import Profile,Image,Comment
+
 
 class SearchForm(forms.Form):
     search_field = forms.CharField(label='', max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Search people...'}))
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude =['likes','profile']
