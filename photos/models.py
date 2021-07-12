14591bc22421771a2b0db6a64eacb4415dfd099f
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-12 17:05:19 +0300

    Update models

--- photos/models.py
+++ photos/models.py
@@ -1,49 +1,113 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime as dt
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
-from django.db.models.fields import CharField, TextField
-from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
-class Profile(models.Model):
-  profile_photo = CloudinaryField('photo')
-  username = models.OneToOneField(User,on_delete = models.CASCADE)
-  bio = models.TextField()
-
-  def profile_save(self):
-    self.save()
-
-  def delete_profile(self):
-    self.delete()
-
-  @classmethod
-  def get_by_id(cls, id):
-    profile = Profile.objects.get(username=id)
-    return profile
-
-  @classmethod
-  def get_profile_by_username(cls, username):
-    profiles = cls.objects.filter(username__contains=username)
-    return profiles
-
-  def __str__(self):
-    return self.username
 
 class Image(models.Model):
   photo = CloudinaryField('image')
-  photo_name = CharField(max_length=30)
-  photo_caption = models.TextField()
+  photo_name = models.CharField(max_length=60)
   posted_at = models.DateTimeField(auto_now_add=True)
+  photo_caption = models.TextField()
   user = models.ForeignKey(User,on_delete = models.CASCADE)
 
-class Comment(models.Model):
-  pass
+  def save_photo(self):
+     self.save()
 
-class Like(models.Model):
-  pass
+  @classmethod
+  def display_photos(cls):
+    photos = cls.objects.all()
+    return photos
+
+  @property
+  def all_comments(self):
+    return self.comments.all()
+
+  @property
+  def all_likes(self):
+    return self.photolikes.count()
+
+  @classmethod
+  def search_photos(cls,search_term):
+    photos = cls.objects.filter(name__icontains = search_term).all()
+    return photos
+
+  def delete_post(self):
+    self.delete()
+
+  def __str__(self):
+    return "%s photo" % self.name
+
+class Profile(models.Model):
+  profile_photo = CloudinaryField('photo')
+  bio = models.TextField()
+  user = models.OneToOneField(User,on_delete = models.CASCADE)
+
+
+  @receiver(post_save , sender = User)
+  def create_profile(instance,sender,created,**kwargs):
+    if created:
+      Profile.objects.create(user = instance)
+
+  @receiver(post_save,sender = User)
+  def save_profile(sender,instance,**kwargs):
+    instance.profile.save()
+
+  @property
+  def all_followers(self):
+    return self.followers.count()   
+
+  @property
+  def all_following(self):
+    return self.following.count() 
+
+
+  @property
+  def follows(self):
+    return [follow.followee for follow in self.following.all()]
+
+  @property
+  def following(self):
+    return self.followers.all()
+
+
+
+  @classmethod
+  def search_profiles(cls,search_term):
+    profiles = cls.objects.filter(user__username__icontains = search_term).all()
+    return profiles
+
+  def __str__(self):
+    return "%s profile" % self.user
+
 
 class Follows(models.Model):
-  pass
+  follower = models.ForeignKey(Profile, related_name='following',on_delete = models.CASCADE)
+  followee = models.ForeignKey(Profile, related_name='followers',on_delete = models.CASCADE)
+
+  def __str__(self):
+    return "%s follower" % self.follower
+
+class Like(models.Model):
+  like = models.BooleanField()
+  photo = models.ForeignKey(Image, on_delete = models.CASCADE,related_name='photolikes')
+  user = models.ForeignKey(User,on_delete = models.CASCADE,related_name='userlikes')
+
+  def __str__(self):
+    return "%s like" % self.photo
+
+class Comment(models.Model):
+  comment = models.TextField()
+  photo = models.ForeignKey(Image,on_delete = models.CASCADE,related_name='comments')
+  user = models.ForeignKey(User,on_delete = models.CASCADE,related_name='comments')
+
+  @classmethod
+  def display_comments_by_photoId(cls,photo_id):
+    comments = cls.objects.filter(photo_id = photo_id)
+    return comments
+
+  def __str__(self):
+    return "%s comment" % self.photo
 
 
