28011ff5dad3a70dde4371f3f22cf2f2ea48eafd
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-13 11:26:30 +0300

    Deployment

--- photos/tests.py
+++ photos/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Image,Profile,Comment
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
